9221f29362018565d3859b9f533bb9fceb3bbc92
kbmanikandan91 <69669627+kbmanikandan91@users.noreply.github.com>
2021-01-02 17:05:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,114 @@
+var score = [0,1,2,3,4,5,6];
+
+var team1={
+    
+    name: "CSK",
+    runs: [],
+    score:0
+}
+
+var team2={
+  
+    name: "Mumbai Indians",
+    runs: [],
+    score:0
+}
+
+var toss;
+
+
+
+window.onload = () =>{
+    selectToss(); 
+    updateNames(); 
+    updateScore();
+}
+
+
+var selectToss = () =>{
+    toss = Math.round(Math.random())+1;
+    console.log(toss);
+}
+
+// Function to change the button text
+var updateButtonText = () =>{
+    var button = document.getElementById("strike-button");
+    console.log(button);
+    var result = document.getElementById("result");
+    result.style.visibility ="";
+    // check if game is over
+    if(team1.runs.length == 6 && team2.runs.length == 6){
+        button.remove();
+        
+        result.textContent = team1.score === team2.score ? `Its a draw`: `${team1.score > team2.score? team1.name:team2.name} Wins`;
+    }
+    else{
+      
+        toss = team1.runs.length ===6 ? 2 : team2.runs.length === 6 ? 1 : toss;
+    }
+
+
+    button.textContent = `${toss === 1 ? team1.name:team2.name} Batting`;
+};
+
+
+var updateNames = () =>{
+    document.getElementById("team-1-name").textContent = team1.name;
+    document.getElementById("team-2-name").textContent = team2.name;
+}
+
+var updateScore = () =>{
+    document.getElementById("team-1-score").textContent = team1.score;
+    document.getElementById("team-2-score").textContent = team2.score;
+    updateRuns(); 
+}
+
+var handleStrikeButtonClick = () =>{
+ var runs = score[Math.floor(Math.random()*score.length)];
+ console.log(runs);
+
+ runs = runs === 5?"W": runs; 
+ console.log(runs);
+
+ 
+ if (toss ===1)
+   {
+    team1.runs.push(runs);
+   team1.score= calculateScore(team1.runs); 
+    
+   } 
+   else{
+    team2.runs.push(runs);
+    team2.score = calculateScore(team2.runs);
+   }
+
+   updateButtonText();
+   updateScore();
+}
+
+var calculateScore = (runs) =>{
+console.log("Calculate score method");
+
+return runs.map(num =>{
+    
+return num =='W'? 0: num;
+
+}).reduce((total,num) => total + num
+
+);
+
+};
+        
+
+
+var updateRuns = () =>{
+    var teamOneRunsElement = document.getElementById("team-1-round-runs").children;
+    var teamTwoRunsElement = document.getElementById("team-2-round-runs").children;
+    // update runs on score board
+    team1.runs.forEach((run,index)=>{
+        teamOneRunsElement[index].textContent = run;
+    });
+    team2.runs.forEach((run,index)=>{
+        teamTwoRunsElement[index].textContent = run;
+    });
+};
